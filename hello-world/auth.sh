6fe7fd08f8b8cb7fb420c75f3367d05f70f7d796
isaac <535080691@qq.com>
2019-11-08 20:44:52 +0800

    add script location

--- hello-world/auth.sh
+++ hello-world/auth.sh
@@ -7,3 +7,10 @@ ls -l auth.sh
 chmod 755 auth.sh
 echo '>>>>>'
 ls -l auth.sh
+
+#where is bash?
+#mkdir bin   mv sh bin   export PATH=~/bin:"$PATH"
+#trandition bash in /usr/local/bin
+#admin bash in /usr/local/sbin
+#local software bash in /usr/local
+#Linux publisher bash in /bin or /usr/bin
